10229177ed48fa2dcb92e9c1a7ed002f2b174778
Sivmey <sivmey.keo@student.cadt.edu.kh>
2024-12-19 18:29:24 +0700

    display room detail&read from file

--- Room.hpp
+++ Room.hpp
@@ -11,12 +11,10 @@ class Room
 private:
     // Unique room ID
     int roomID;
-    // 1 mean room is available and 0 room unavailable
-    bool available;
-    // price of the room
-    double price;
-    // typs of room like 1bed or 2beds
+    // types of room like 1bed or 2beds
     std::string roomType;
+    //price of the room
+    double price;
 
 public:
     // Constructor
@@ -24,20 +22,53 @@ public:
     {   this->roomID = roomID;
         this->price = price;
         this->roomType = type;
-        this->available = 1;   // Default to available
+    }
+    
+    // Default constructor
+    Room() : roomID(0), price(0.0), roomType("") {}
+
+    // Display room info (price & type)
+    void displayRoomDetails()
+    {
+        std::cout << "Room ID: " << roomID << ", Price: $" << price << ", Room Type: " << roomType <<std::endl;
     }
 
-
-    // display all rooms that available
-    void isRoomAvailable()
+    // display room info( price & type) read from file
+    void roomDetail(const std::string &filename)
     {
+        std::ifstream file(filename);
 
-    }
+        if (!file.is_open())
+        {
+            std::cerr << "Error: Unable to open file " << filename << "\n";
+            return;
+        }
 
-    // display room info( price & type)
-    void roomDetail(int roomID, double price, std::string &type)
-    {
-        
+        std::string line;
+        while (std::getline(file, line))
+        {
+            std::istringstream iss(line);
+            int id;
+            std::string type;
+            double cost;
+
+            if (iss >> id >> type >> cost)
+            {
+                roomID = id;
+                roomType = type;
+                price = cost;
+
+                // Display the room details
+                displayRoomDetails();
+            }
+            else
+            {
+                std::cerr << "Error: Invalid data format in file." << "\n";
+            }
+        }
+
+        file.close();
+
     }
 
    
